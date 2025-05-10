b093a43f72762bbe05eff27ff90271e49b98c027
dongjun <dongjuncoding@gmail.com>
2025-05-10 11:47:32 +0800

    test creating window

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include<SDL2/SDL.h>
 
 int main(int argc, char* argv[]) {
+  
+  // Create a window data type
+  SDL_Window* window=nullptr;
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     std::cout << "SDL could not be initialised: " <<
@@ -14,6 +17,16 @@ int main(int argc, char* argv[]) {
     std::cout << "SDL video system is ready to go.\n";
   }
 
+
+  window = SDL_CreateWindow("Spaced Out",
+      0,
+      0,
+      640,
+      480,
+      SDL_WINDOW_SHOWN);
+  SDL_Delay(3000);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
 
 }
