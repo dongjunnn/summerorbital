e6da70b35756d8ee6152ae1f41d582492494c99d
dongjunnn <dongjunnn@users.noreply.github.com>
2025-06-11 21:19:51 +0800

    setup server

--- backend/Client/main.cpp
+++ backend/Client/main.cpp
@@ -13,15 +13,15 @@ int main(int argc, char ** argv)
     ENetHost* client;
     client = enet_host_create(NULL, 1, 1, 0, 0);
 
-    if (clinet == NULL)
+    if (client == NULL)
     {
-        fprintf(stderr, "An error occured while creating the client");
+        fprintf(stderr, "An error occurred while creating the client");
         return EXIT_FAILURE;
     }
 
     ENetAddress address;
     ENetEvent event;
-    ENetPeer* peer; // enet peer is the host i think
+    ENetPeer* peer;
 
     enet_address_set_host(&address, "127.0.0.1");
     address.port = 7777;
@@ -30,8 +30,6 @@ int main(int argc, char ** argv)
     if (peer == NULL) 
     {
         fprintf(stderr, "No available peers for initiating an ENet connection."); 
-        // This function sends formatted text to the given file stream ie the error stream which
-        // is separated from the standard output stream
         return EXIT_FAILURE;
     }
 
@@ -48,16 +46,37 @@ int main(int argc, char ** argv)
     }
 
     // GAME LOOP START
-
-    while (enet_host_service(client, &event, 10) > 0) // pass by pointer with &
+    while (enet_host_service(client, &event, 10) > 0)
     {
         switch(event.type)
         {
             case ENET_EVENT_TYPE_RECEIVE:
-                
+                printf("A packet of length %u containing %s was received from %x:%u on channel %u.\n",
+                    event.packet->dataLength,
+                    event.packet->data,
+                    event.peer->address.host,
+                    event.peer->address.port,
+                    event.channelID);
+                break;
         }
     }
     // GAME LOOP END
 
+    enet_peer_disconnect(peer, 0);  // <- fixed: missing semicolon
 
+    while (enet_host_service(client, &event, 3000) > 0)
+    {
+        switch (event.type)
+        {
+            case ENET_EVENT_TYPE_RECEIVE:
+                enet_packet_destroy(event.packet);
+                break;
+        
+            case ENET_EVENT_TYPE_DISCONNECT:
+                puts("Disconnection succeeded");
+                break;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
